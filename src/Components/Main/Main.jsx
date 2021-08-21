9a36f184e2750d2692e23c4659b0d6f300bdae09
rach-con13 <rachaelconcessio+@gmail.com>
2021-08-21 09:05:24 -0400

    updated redirect

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
-import { Route, Switch, useHistory } from "react-router-dom";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import UseFirebaseUser from "src/Globals/Hooks/Firebase/useFirebaseUser";
 import Login from "../Auth/Login";
 import SignUp from "../Auth/SignUp";
@@ -12,16 +14,32 @@ import Surveys from "../Surveys/Surveys";
 export default function Main() {
   const history = useHistory();
   const { user } = UseFirebaseUser();
+  const [userID, setUserID] = useState(null);
   useEffect(() => {
-    if (user) {
-      history.push(`/surveys/${user.uid}`);
-    }
-  }, []);
+    const isLoggedIn = async () => {
+      const userStatus = await user;
+      userStatus && setUserID(userStatus.uid);
+    };
+    isLoggedIn();
+  });
+  // useEffect(() => {
+  //   console.log(user);
+  //   if (user) {
+  //     history.push(`/surveys/${user.uid}`);
+  //   }
+  // }, [user]);
   return (
     <>
       <div className="App">
         <Header />
         <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return userID && <Redirect to={`/surveys/${userID}`} />;
+            }}
+          ></Route>
           <Route path="/responses/:id">
             <>
               <SurveyContent>
